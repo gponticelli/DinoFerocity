ad2c2be50ebbfe45246e1dad7478dcaa06733140
gponticelli <gabriel@ponticelli.me>
2017-02-20 16:22:41 -0700

    added weight and ferocity

--- dinosaurs.js
+++ dinosaurs.js
@@ -1,3 +1,11 @@
 var dinosaurs = [
-    { name: "triceratops" }
-]
+  { name: "Triceratops", ferocity: 0 },
+  { name: "Velociraptor", ferocity: 0 },
+  { name: "Gigantasaurus", ferocity: 0 },
+  { name: "Dilophisauraus", ferocity: 0 },
+  { name: "Pachycefelasaurus", ferocity: 0 },
+  { name: "Compsognathys", ferocity: 0 },
+  { name: "Carnatasaurus", ferocity: 0 },
+  { name: "Stegasaurus", ferocity: 0 },
+  { name: "Ankylosaurus", ferocity: 0 }
+];
